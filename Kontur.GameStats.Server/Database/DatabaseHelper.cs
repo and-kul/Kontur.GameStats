d78c6855293b68d1e0e78317e1a3923e1efdbad8
Andrey Kulagin <mr.and.kul@gmail.com>
2017-02-26 15:08:52 +0300

    Add PutServerInfoModule and DatabaseHelper

---
+++ Kontur.GameStats.Server/Database/DatabaseHelper.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Kontur.GameStats.Server.Models;
+using Kontur.GameStats.Server.NancyModules;
+using Nancy;
+
+namespace Kontur.GameStats.Server.Database
+{
+    static class DatabaseHelper
+    {
+        public static GameMode FindGameMode(string gameModeName, GameStatsDbContext db)
+        {
+            var gameMode = db.GameModes.FirstOrDefault(gm => gm.Name == gameModeName);
+
+            if (gameMode != null) return gameMode;
+
+            gameMode = new GameMode { Name = gameModeName };
+            db.GameModes.Add(gameMode);
+            db.SaveChanges();
+
+            return gameMode;
+        }
+
+        
+        public static void AddNewServer(ServerInfo serverInfo, GameStatsDbContext db)
+        {
+            var server = new Models.Server
+            {
+                Endpoint = serverInfo.Endpoint,
+                Name = serverInfo.Name,
+                Statistics = new ServerStatistics()
+            };
+            db.Servers.Add(server);
+
+            foreach (var gameModeName in serverInfo.GameModes)
+            {
+                var gameMode = FindGameMode(gameModeName, db);
+
+                var serverGameMode = new ServerGameMode
+                {
+                    Server = server,
+                    GameMode = gameMode
+                };
+
+                db.ServersGameModes.Add(serverGameMode);
+            }
+
+            db.SaveChanges();
+        }
+
+
+        public static void RemoveServer(Models.Server server, GameStatsDbContext db)
+        { 
+            db.Servers.Remove(server);
+            db.SaveChanges();
+        }
+
+
+        public static void UpdateExistingServer(Models.Server server, ServerInfo newServerInfo, GameStatsDbContext db)
+        {
+            RemoveServer(server, db);
+            AddNewServer(newServerInfo, db);
+        }
+
+
+        public static void AddOrUpdateServer(ServerInfo serverInfo)
+        {
+            using (var db = new GameStatsDbContext())
+            {
+                var server = db.Servers.FirstOrDefault(s => s.Endpoint == serverInfo.Endpoint);
+
+                if (server == null)
+                {
+                    AddNewServer(serverInfo, db);
+                }
+                else
+                {
+                    UpdateExistingServer(server, serverInfo, db);
+                }
+                
+            }
+        }
+
+
+
+
+
+    }
+}
---
+++ Kontur.GameStats.Server/NancyModules/PutServerInfoModule.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Kontur.GameStats.Server.Database;
+using Kontur.GameStats.Server.Models;
+using Nancy;
+using Nancy.ModelBinding;
+
+namespace Kontur.GameStats.Server.NancyModules
+{
+    class ServerInfo
+    {
+        public string Endpoint;
+        public string Name;
+        public string[] GameModes;
+    }
+
+
+
+    public class PutServerInfoModule : NancyModule
+    {
+        
+        public PutServerInfoModule()
+        {
+            Put["/servers/{endpoint}/info"] = parameters =>
+            {
+                var serverInfo = this.Bind<ServerInfo>();
+
+                DatabaseHelper.AddOrUpdateServer(serverInfo); 
+
+                return HttpStatusCode.OK;
+            };
+        }
+    }
+}
